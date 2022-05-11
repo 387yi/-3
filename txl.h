c059670e74cc2346f283b00e56e0e17efb4f8eb4
387yi <103522686+387yi@users.noreply.github.com>
2022-05-11 13:53:59 +0800

    Add files via upload

---
+++ txl.h
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<errno.h>
+
+//#define MAX 100
+#define MAX_NAME 10
+#define MAX_DIANHUA 20
+#define MAX_SEX 5
+#define MAX_DIZI 20
+#define REN 3
+
+
+
+typedef struct s {
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char dianhua[MAX_DIANHUA];
+	char dizi[MAX_DIZI];
+}sd;
+typedef struct ps {
+	struct s* renshu;
+	int size;
+	int cont;
+}ps;
+enum s1 {
+	tuichu,
+	tianjia,
+	shanchu,
+	xiugai,
+	chazhao,
+	chakan,
+	baocun
+};
+//初始化数据
+void Contacth(struct ps* s);
+//添加数据
+void Contacttj(struct ps* s);
+//删除数据
+void Contactsc(struct ps* s);
+//修改数据
+void Contactxg(struct ps* s);
+//查找数据
+void Contactcz(const struct ps* s);
+//查看所以数据
+void Contactck(const struct ps* s);
+//退出程序，归还动态内存
+void Contacttc(struct ps* s);
+//保持在文件中
+void Contactbc(struct ps* s);
+//提取文件中的数据
+void Contactcs(struct ps* s);
